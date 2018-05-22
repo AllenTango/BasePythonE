7ed7c0477745a790e85855fd9ea3a53111ba53d4
allen <allentango@163.com>
2018-05-22 18:33:30 +0800

    绘制七段数码管， done()需了解

---
+++ SevenDigitsDrawV1.py
@@ -0,0 +1,49 @@
+# 七段数码管绘制 基本思路
+# 1. 绘制单个数字对应的数码管
+# 2. 获得一串数字，绘制对应的数码管
+# 3. 获得当前系统时间，绘制对应的数码管
+
+import turtle as t
+
+
+# import time
+
+# 绘制单段数码管
+def drawLine(draw):
+    t.pendown() if draw else t.penup()
+    t.fd(40)
+    t.right(90)
+
+
+# 根据数字绘制七段数码管
+def drawDigit(digit):
+    drawLine(True) if digit in [2, 3, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 1, 3, 4, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 3, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 6, 8] else drawLine(False)
+    t.left(90)
+    drawLine(True) if digit in [0, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 3, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 1, 2, 3, 4, 7, 8, 9] else drawLine(False)
+    t.left(180)
+    t.penup()  # 为绘制后续数字确定位置
+    t.fd(20)  # 为绘制后续数字确定位置
+    # t.done()   这不是随便用的，需要在主函数调用时，最好不要出现哦
+
+# 获取要输出的数字，通过eval()函数将数字转变为整数
+def drawDate(date):
+    for i in date:
+        drawDigit(eval(i))
+
+# 组合模块
+def main():
+    t.setup(800, 400, 200, 200)
+    t.penup()
+    t.fd(-300)
+    t.pensize(5)
+    drawDate('20181010')
+    t.hideturtle()
+    t.done()
+
+
+main()
