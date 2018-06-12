0a7efbfb1ebd9d8ca03cb1541f13501726b96162
allen <allentango@163.com>
2018-06-12 11:25:25 +0800

    词云中文处理，需添加中文字体路径font_path="Sonti.ttc"

--- GovRptWordCloudv2T4.py
+++ GovRptWordCloudv2T4.py
@@ -1,4 +1,4 @@
-# GovRptWordCloudv2.py
+#GovRptWordCloudv2.py
 # 步骤1：读取文件、分词整理
 # 步骤2：设置并输出词云
 # 步骤3：观察结果，优化迭代
@@ -6,18 +6,19 @@
 import jieba
 import wordcloud
 from scipy.misc import imread
-
 mask = imread("chinamap.jpg")
-excludes = {}
+excludes = { }
 f = open("关于实施乡村振兴战略的意见.txt", "r", encoding="utf-8")
 t = f.read()
 f.close()
 ls = jieba.lcut(t)
 txt = " ".join(ls)
-w = wordcloud.WordCloud( \
-    width=1000, height=700, \
-    background_color="white",
-    font_path="Songti.ttc", mask=mask
-)
+w = wordcloud.WordCloud(\
+    width = 1000, height = 700,\
+    background_color = "white",
+    font_path = "msyh.ttc", mask = mask
+    )
 w.generate(txt)
 w.to_file("grwordcloudm.png")
+
+
